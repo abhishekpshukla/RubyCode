145c5b3c5afad158112b11056d56c72b5fdddf75
Abhishek Shukla <abhishekpshukla@gmail.com>
2019-02-14 00:39:28 +0530

    Updated README.md file - added a step to run the spec.

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -2,7 +2,7 @@ require_relative '../main'
 
 describe Main do
   it "should return valid value" do
-  	expect(Main.new.test).to eq("I am test")
-		expect(Main.class_method_one).to eq("This is class Method")
+    expect(Main.new.test).to eq("I am test")
+    expect(Main.class_method_one).to eq("This is class Method")
   end
 end
